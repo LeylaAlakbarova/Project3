9c29b934e87efc5d245d9a7648f27a12b27bee09
Leyla Alakbarova <85068991+LeylaAlakbarova@users.noreply.github.com>
2023-11-15 23:50:16 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -48,6 +48,14 @@ function changeComma(e) {
 }
 
 function exchangeCurrency() {
+  currencyFirst.value = currencyFirst.value.replace(/ /g, "");
+
+  if (
+    (currencyFirst.value.indexOf(",") == -1 || currencyFirst.value.indexOf(".") == -1) && currencyFirst.value.match(/[a-z&\/\\_^#@+()$~%'"`!|:*?<>{}-]/g)
+  ) {
+    currencyFirst.value = "";
+    currencySec.value = "";
+  } else {
     if (
       currencyFirst.value.indexOf(",") == -1 &&
       currencyFirst.value.indexOf(".") == -1
@@ -74,7 +82,7 @@ function exchangeCurrency() {
       }
     });
   }
-// }
+}
 
 function exchangeFrom(e) {
   currencyService.changevalue1(e.target.textContent);
